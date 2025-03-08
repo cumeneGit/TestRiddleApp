12ed045bd818f93e7d48157002cfc746bbcdf60b
cumene <cumene.dev@gmail.com>
2025-03-08 22:07:12 +0900

    Initial commit

---
+++ TestRiddle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestRiddle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
