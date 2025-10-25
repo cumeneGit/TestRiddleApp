ca786e0629f1463395734d992b9e8c5f367c3b2a
cumeneGit <cumene.dev@gmail.com>
2025-10-25 18:10:50 +0900

    test

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
 
@@ -6,3 +6,18 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World2\n');
 }).listen(port);
+*/
+// server/server.js
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello World from server!' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
