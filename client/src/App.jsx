83460767013ac8afd8982aff7d19650515da401b
cumeneGit <cumene.dev@gmail.com>
2025-10-25 19:35:56 +0900

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
 
   // server の API を呼ぶ
   useEffect(() => {
-    fetch('http://testriddleapp-server.onrender.com/api/hello')
+    fetch('https://testriddleapp-server.onrender.com/api/hello')
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error(err));
